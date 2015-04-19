3ffbd670e92ba39568c3a706dbe7542e20ea7658
Faizan Naqvi <faizan@nerdiacs.com>
2015-04-20 00:42:14 +0200

    Update binary_tree_amplitude.py

--- binary_tree_amplitude.py
+++ binary_tree_amplitude.py
@@ -42,22 +42,18 @@ tree = bintree(5,
 
 def maxdiff(maxN,minN, node):
 
-    if node.l == None and node.r == None:
-        maxN = maxN if maxN >= node.x else node.x
-        minN = minN if minN <= node.x else node.x
-        return (maxN,minN)
-    #diffMax = abs(maxN-minN)
+    if node == None:
+        return (maxN, minN)
+
     maxN1 = maxN if maxN != None and maxN >= node.x  else node.x # node.x is max if maxN is less than it
     minN1 = minN if minN != None and minN <= node.x  else node.x # node.x is min if minN is > than it
-    #print "max,min %d,%d" % (maxN1,minN1)
+    
     maxN2 = maxN1
     minN2 = minN1
     # split paths here
-    if node.l != None:
-        (maxN1,minN1) = maxdiff(maxN1,minN1,node.l)
+    (maxN1,minN1) = maxdiff(maxN1,minN1,node.l)
 
-    if node.r != None:
-        (maxN2,minN2) = maxdiff(maxN2,minN2,node.r)
+    (maxN2,minN2) = maxdiff(maxN2,minN2,node.r)
 
     if abs(maxN1-minN1) > abs(maxN2 - minN2):
         return (maxN1,minN1)
